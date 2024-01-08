369af9a428ddf7cdd168425e7a097b3e1458ca53
originNis <1091826264@qq.com>
2024-01-08 21:31:40 +0800

    added ResponseStatus code to address more different error

--- beans/src/main/java/com/rybin/cybermall/beans/ResponseStatus.java
+++ beans/src/main/java/com/rybin/cybermall/beans/ResponseStatus.java
@@ -3,4 +3,8 @@ package com.rybin.cybermall.beans;
 public class ResponseStatus {
     public static final int SUCCESS = 10000;
     public static final int FAIL = 10001;
+
+    public static final int NOT_LOGIN = 20001;
+    public static final int INVALID_TOKEN = 20002;
+    public static final int LOGIN_OVERDUE = 20003;
 }
